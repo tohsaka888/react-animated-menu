87296e50f19b41bc0f2f80b78535a13c5d03f302
tohsaka888 <156132264@qq.com>
2022-06-02 11:03:51 +0800

    feat: 新增activeItemContext

--- src/components/Context.ts
+++ src/components/Context.ts
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, MutableRefObject } from "react";
 import { MenuContextProps } from "./type";
 
 export const MenuContext = createContext<MenuContextProps | null>(null)
+export const ActiveItemContext = createContext<MutableRefObject<HTMLDivElement> | null>(null)
