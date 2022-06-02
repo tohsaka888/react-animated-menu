a88f4caf02faf81c4cee2c470787ab2205c27437
tohsaka888 <156132264@qq.com>
2022-06-02 09:40:20 +0800

    feat: 示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AnimatedMenu from './components/AnimatedMenu';
+import { MenuItemProps } from './components/type';
+
+const items: MenuItemProps[] = [
+  {
+    label: 'HomePage',
+    key: 'index',
+  },
+  {
+    label: 'Music',
+    key: 'music'
+  },
+  {
+    label: 'News',
+    key: 'news'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AnimatedMenu items={items} itemStyle={{ padding: '8px 12px' }} defaultSelectedKeys={['news']} />
+    </>
   );
 }
 
